cc7e8ccc91aeaa5b6f6e99e2f67cc250dff01b53
Kevin Romero Peces-Barba <kevinrpb@hotmail.com>
2021-01-10 16:22:44 +0100

    Fix hyper/es-hyper configs and set substrates for acrobot

--- util/substrate.py
+++ util/substrate.py
@@ -4,14 +4,27 @@ from util.model import EnvKind, ModelKind
 
 
 def _acrobot_hyperneat():
-  return None
+  input_coordinates = []
+  for i in range(0, 6):
+    input_coordinates.append((-1. + (2.*i/3.), -1.))
+
+  hidden_coordinates = [[(-0.5, 0.5), (0.5, 0.5)], [(-0.5, -0.5), (0.5, -0.5)]]
+  output_coordinates = [(-1., 1.), (1., 1.)]
+
+  return Substrate(input_coordinates, output_coordinates, hidden_coordinates)
 
 def _acrobot_es_hyperneat():
-  return None
+  input_coordinates = []
+  for i in range(0, 6):
+    input_coordinates.append((-1. +(2.*i/3.), -1.))
+
+  output_coordinates = [(-1., 1.), (1., 1.)]
+
+  return Substrate(input_coordinates, output_coordinates)
 
 def _cartpole_hyperneat():
   input_coordinates = []
-  for i in range(0,4):
+  for i in range(0, 4):
     input_coordinates.append((-1. + (2.*i/3.), -1.))
 
   hidden_coordinates = [[(-0.5, 0.5), (0.5, 0.5)], [(-0.5, -0.5), (0.5, -0.5)]]
@@ -21,7 +34,7 @@ def _cartpole_hyperneat():
 
 def _cartpole_es_hyperneat():
   input_coordinates = []
-  for i in range(0,4):
+  for i in range(0, 4):
     input_coordinates.append((-1. +(2.*i/3.), -1.))
 
   output_coordinates = [(-1., 1.), (1., 1.)]
